4c2ba9c963e4c94f1a767901aa8d2cf65a72d6c5
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2025-05-17 18:31:05 +0700

    Init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+)
+
+func main() {
+  r := gin.Default()
+  r.LoadHTMLFiles("templates/index.tmpl")
+  r.GET("/index", func(c *gin.Context) {
+    c.HTML(http.StatusOK, "index.tmpl", gin.H{
+        "title": "SpoYT",
+    })
+  })
+  r.Run()
+}
