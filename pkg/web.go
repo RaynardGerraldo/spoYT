fe4c5bceb75a40c7dd11edad52b9cd4a1174b259
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2025-06-27 17:32:55 +0700

    feat: usable web version

--- pkg/web.go
+++ pkg/web.go
@@ -2,16 +2,38 @@ package pkg
 
 import (
   "net/http"
+  "spoyt/util"
+  "path/filepath"
   "github.com/gin-gonic/gin"
 )
 
 func Web() {
   r := gin.Default()
   r.LoadHTMLFiles("templates/index.tmpl")
-  r.GET("/index", func(c *gin.Context) {
+  r.GET("/", func(c *gin.Context) {
     c.HTML(http.StatusOK, "index.tmpl", gin.H{
         "title": "SpoYT",
     })
   })
+
+  r.POST("/upload", func(c *gin.Context) {
+    file, err := c.FormFile("file")
+    if err != nil {
+	    c.String(http.StatusBadRequest, "File upload error: %v", err)
+		return
+	}
+
+    dst := filepath.Join("uploads", filepath.Base(file.Filename))
+    if err := c.SaveUploadedFile(file, dst); err != nil {
+        c.String(http.StatusInternalServerError, "Save failed: %v", err)
+	    return
+    }
+    util.Converter(dst)
+
+    // TODO
+    // Progress Bar
+    // Playlist link output (maybe generate and grab TL link directly?)
+  })
+
   r.Run()
 }
